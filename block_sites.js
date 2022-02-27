1d7893e050e442a7bdcf1dbb2b1b868d12594733
jordansafer <jordansafer2@gmail.com>
2022-02-27 17:16:01 -0500

    Blocked sites minus sites list

---
+++ block_sites.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Block sites
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @grant        none
+// @run-at: document-start
+// ==/UserScript==
+
+const metricsToRead = JSON.parse(localStorage.getItem("blockedSiteData") || "{}")
+var visits = metricsToRead.visits || 0
+
+function reportMetrics() {
+    const metricsToStore = {
+        "visits": visits,
+        "timeRemaining": 0,
+        "visitMap": {}
+    }
+    localStorage.setItem("blockedSiteData", JSON.stringify(metricsToStore))
+}
+
+function leaveBlockedSites() {
+    const bannedSiteRegexps = []
+    const bannedRegexp = new RegExp(`.*${bannedSiteRegexps.join(".*|.*")}.*`)
+
+    const curUrl = window.location.href
+    if (bannedRegexp.test(curUrl)) {
+        visits++
+        console.log(`The site ${curUrl} is blocked`)
+        reportMetrics()
+        alert(`The site ${curUrl} is blocked. \nblocked site visits: ${visits}`)
+        1/0
+        location.href = "https://localhost"
+    }
+}
+
+leaveBlockedSites()
+
+
+
